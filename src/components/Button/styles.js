55655f4eee890f39ff8d2f2511cc54c464cdc6ee
felipinas <jfss2@cin.ufpe.br>
2022-04-30 10:33:43 -0300

    feat: add the button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    width: ${props => props.width || '100%'};
+    border: none;
+    border-radius: 26px;
+
+    padding: 12px 12px;
+    
+    background-color: #C53F3F;
+    color: white;
+    font-weight: bold;
+    font-size: 20px;
+
+    cursor: pointer;
+
+    &:disabled {
+        background-color: #DC8C8C;
+        cursor: not-allowed;
+    }
+`;
