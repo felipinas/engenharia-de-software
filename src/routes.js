0390701a73010b3f47b8e9b6a9acc0a27b18bcbd
felipinas <jfss2@cin.ufpe.br>
2022-05-13 17:44:56 -0300

    feat: add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,46 @@
-import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes as Switch
+}
+from "react-router-dom";
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Feed from "./pages/Feed";
+
+import { useAuth } from './contexts/AuthContext';
+
+function PrivateRoute({ children }) {
+  const { currentUser } = useAuth();
+  
+  if (!currentUser) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
 
 function Routes() {
+  const { currentUser } = useAuth();
+
   return (
     <BrowserRouter>
       <Switch>
 
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={!currentUser ? <Home /> : <Navigate to="/feed" />} />
 
-        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/login" element={!currentUser ? <Login /> : <Navigate to="/feed" />} />
 
-        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/signup" element={!currentUser ? <SignUp /> : <Navigate to="/feed" />} />
+
+        <Route exact path="/feed" element={
+          <PrivateRoute>
+            <Feed />
+          </PrivateRoute>
+        } />
 
       </Switch>
     </BrowserRouter>
