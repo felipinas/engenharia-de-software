aabde9649fafeeca2438dcfad77f080c2c36bbb8
felipinas <jfss2@cin.ufpe.br>
2022-05-16 00:26:47 -0300

    feat: create the my rides page

---
+++ src/pages/MyRides/index.js
@@ -0,0 +1,17 @@
+import Header from '../../components/Header';
+import { Container } from './styles';
+
+function MyRides() {
+  return (
+      <>
+        <Header />
+        <Container>
+            <section>
+              <h1>Minhas caronas</h1>
+            </section>
+        </Container>
+      </>
+  );
+}
+
+export default MyRides;
---
+++ src/pages/MyRides/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    section {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        padding: 30px 0 20px 0;
+        margin: 0 16px;
+
+        border-bottom: 1px solid #DBDBDB;
+    }
+`;
