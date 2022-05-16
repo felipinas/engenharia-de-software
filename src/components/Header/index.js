92330dc439fe6a628e10fb20938b3987094d6247
felipinas <jfss2@cin.ufpe.br>
2022-05-16 00:27:50 -0300

    feat: adapt header to new pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,32 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Squash as Hamburger } from 'hamburger-react'
+
+import { useAuth } from '../../contexts/AuthContext';
 
 import { Container, Nav } from './styles';
 
 import homeIcon from '../../assets/icons/home.svg';
 import profileIcon from '../../assets/icons/user.svg';
 import carIcon from '../../assets/icons/car.svg';
+import logoutIcon from '../../assets/icons/logout.svg';
 
 function Header() {
     const [isNavOpen, setIsNavOpen] = useState(false);
 
     const onClickMenu = () =>  setIsNavOpen(state => !state);
+
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+        await logout();
+        navigate('/')
+        } catch (error) {
+        console.log(error);
+        }
+    };
 
     return (
         <>
@@ -38,6 +53,7 @@ function Header() {
                                         Ínicio
                                     </Link>
                                 </li>
+
                                 <li>
                                     <img src={profileIcon} alt="" />
                                     <Link to="/profile">
@@ -45,11 +61,19 @@ function Header() {
                                     </Link>
                                     
                                 </li>
+
                                 <li>
                                     <img src={carIcon} alt="" />
                                     <Link to="/my-rides">
                                         Minhas caronas
                                     </Link>
+                                </li>
+                                
+                                <li onClick={handleLogout}>
+                                    <img src={logoutIcon} alt="" />
+                                    <p>
+                                        Deslogar
+                                    </p>
                                 </li>
                             </ul>
                         </Nav>
